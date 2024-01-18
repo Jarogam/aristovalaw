d7a85e3285da0c58e1cd7c724631bb86eed77587
neXau <c03v23@gmail.com>
2024-01-18 19:55:21 +0200

    Add Subdir logic

--- Paths.php
+++ Paths.php
@@ -6,15 +6,17 @@ define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('SITE_URL', ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST']);
 define('CURRENT_URL', ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 
+define('SUBDIR', '');
+
 // Functions for connecting styles
 function get_css_url($dir) {
-    return SITE_URL . '/pages/assets/css/' . $dir;
+    return SITE_URL . SUBDIR . '/pages/assets/css/' . $dir;
 }
 function get_js_url($dir) {
-    return SITE_URL . '/pages/assets/js/' . $dir;
+    return SITE_URL . SUBDIR . '/pages/assets/js/' . $dir;
 }
 function get_img_url($dir) {
-    return SITE_URL . '/pages/assets/img/' . $dir;
+    return SITE_URL . SUBDIR . '/pages/assets/img/' . $dir;
 }
 
 // Functions for connecting parts of the template
--- Routes.php
+++ Routes.php
@@ -2,76 +2,76 @@
 
 // Check routes for routing
 $routes = [
-    '/' => 'pages/home.php',
-    '/ru/' => 'pages/ru/pages/home.php',
+    '/' => '/pages/home.php',
+    '/ru/' => '/pages/ru/pages/home.php',
 
-    '/test/' => 'pages/test.php',
-    '/test' => 'pages/test.php',
+    '/test/' => '/pages/test.php',
+    '/test' => '/pages/test.php',
 
-    '/ol/' => 'pages/our-locations.php',
-    '/ol' => 'pages/our-locations.php',
-    '/ru/ol/' => 'pages/ru/pages/our-locations.php',
-    '/ru/ol' => 'pages/ru/pages/our-locations.php',
+    '/ol/' => '/pages/our-locations.php',
+    '/ol' => '/pages/our-locations.php',
+    '/ru/ol/' => '/pages/ru/pages/our-locations.php',
+    '/ru/ol' => '/pages/ru/pages/our-locations.php',
 
-    '/lo/' => 'pages/locations.php',
-    '/lo' => 'pages/locations.php',
-    '/ru/lo/' => 'pages/ru/pages/locations.php',
-    '/ru/lo' => 'pages/ru/pages/locations.php',
+    '/lo/' => '/pages/locations.php',
+    '/lo' => '/pages/locations.php',
+    '/ru/lo/' => '/pages/ru/pages/locations.php',
+    '/ru/lo' => '/pages/ru/pages/locations.php',
 
-    '/en/' => 'pages/ebinow.php',
-    '/en' => 'pages/ebinow.php',
-    '/ru/en/' => 'pages/ru/pages/ebinow.php',
-    '/ru/en' => 'pages/ru/pages/ebinow.php',
+    '/en/' => '/pages/ebinow.php',
+    '/en' => '/pages/ebinow.php',
+    '/ru/en/' => '/pages/ru/pages/ebinow.php',
+    '/ru/en' => '/pages/ru/pages/ebinow.php',
 
-    '/eb2e' => 'pages/eb-2-entrepreneurs.php',
-    '/eb2e/' => 'pages/eb-2-entrepreneurs.php',
-    '/ru/eb2e' => 'pages/ru/pages/eb-2-entrepreneurs.php',
-    '/ru/eb2e/' => 'pages/ru/pages/eb-2-entrepreneurs.php',
+    '/eb2e' => '/pages/eb-2-entrepreneurs.php',
+    '/eb2e/' => '/pages/eb-2-entrepreneurs.php',
+    '/ru/eb2e' => '/pages/ru/pages/eb-2-entrepreneurs.php',
+    '/ru/eb2e/' => '/pages/ru/pages/eb-2-entrepreneurs.php',
 
-    '/eb2a' => 'pages/eb-2-artists.php',
-    '/eb2a/' => 'pages/eb-2-artists.php',
-    '/ru/eb2a' => 'pages/ru/pages/eb-2-artists.php',
-    '/ru/eb2a/' => 'pages/ru/pages/eb-2-artists.php',
+    '/eb2a' => '/pages/eb-2-artists.php',
+    '/eb2a/' => '/pages/eb-2-artists.php',
+    '/ru/eb2a' => '/pages/ru/pages/eb-2-artists.php',
+    '/ru/eb2a/' => '/pages/ru/pages/eb-2-artists.php',
 
-    '/eb1imm-fp' => 'pages/eb-1-immigration-filing-process.php',
-    '/eb1imm-fp/' => 'pages/eb-1-immigration-filing-process.php',
-    '/ru/eb1imm-fp' => 'pages/ru/pages/eb-1-immigration-filing-process.php',
-    '/ru/eb1imm-fp/' => 'pages/ru/pages/eb-1-immigration-filing-process.php',
+    '/eb1imm-fp' => '/pages/eb-1-immigration-filing-process.php',
+    '/eb1imm-fp/' => '/pages/eb-1-immigration-filing-process.php',
+    '/ru/eb1imm-fp' => '/pages/ru/pages/eb-1-immigration-filing-process.php',
+    '/ru/eb1imm-fp/' => '/pages/ru/pages/eb-1-immigration-filing-process.php',
 
-    '/eb1c' => 'pages/eb-1-criteria.php',
-    '/eb1c/' => 'pages/eb-1-criteria.php',
-    '/ru/eb1c' => 'pages/ru/pages/eb-1-criteria.php',
-    '/ru/eb1c/' => 'pages/ru/pages/eb-1-criteria.php',
+    '/eb1c' => '/pages/eb-1-criteria.php',
+    '/eb1c/' => '/pages/eb-1-criteria.php',
+    '/ru/eb1c' => '/pages/ru/pages/eb-1-criteria.php',
+    '/ru/eb1c/' => '/pages/ru/pages/eb-1-criteria.php',
 
-    '/eb1li' => 'pages/eb1-list-initial.php',
-    '/eb1li/' => 'pages/eb1-list-initial.php',
-    '/ru/eb1li' => 'pages/ru/pages/eb1-list-initial.php',
-    '/ru/eb1li/' => 'pages/ru/pages/eb1-list-initial.php',
+    '/eb1li' => '/pages/eb1-list-initial.php',
+    '/eb1li/' => '/pages/eb1-list-initial.php',
+    '/ru/eb1li' => '/pages/ru/pages/eb1-list-initial.php',
+    '/ru/eb1li/' => '/pages/ru/pages/eb1-list-initial.php',
 
-    '/im-lp' => 'pages/immigration-law-practice.php',
-    '/im-lp/' => 'pages/immigration-law-practice.php',
-    '/ru/im-lp' => 'pages/ru/pages/immigration-law-practice.php',
-    '/ru/im-lp/' => 'pages/ru/pages/immigration-law-practice.php',
+    '/im-lp' => '/pages/immigration-law-practice.php',
+    '/im-lp/' => '/pages/immigration-law-practice.php',
+    '/ru/im-lp' => '/pages/ru/pages/immigration-law-practice.php',
+    '/ru/im-lp/' => '/pages/ru/pages/immigration-law-practice.php',
 
-    '/im-l' => 'pages/immigration-law.php',
-    '/im-l/' => 'pages/immigration-law.php',
-    '/ru/im-l' => 'pages/ru/pages/immigration-law.php',
-    '/ru/im-l/' => 'pages/ru/pages/immigration-law.php',
+    '/im-l' => '/pages/immigration-law.php',
+    '/im-l/' => '/pages/immigration-law.php',
+    '/ru/im-l' => '/pages/ru/pages/immigration-law.php',
+    '/ru/im-l/' => '/pages/ru/pages/immigration-law.php',
 
-    '/privacy-policy' => 'pages/privacy-policy.php',
-    '/privacy-policy/' => 'pages/privacy-policy.php',
-    '/ru/privacy-policy' => 'pages/ru/pages/privacy-policy.php',
-    '/ru/privacy-policy/' => 'pages/ru/pages/privacy-policy.php',
+    '/privacy-policy' => '/pages/privacy-policy.php',
+    '/privacy-policy/' => '/pages/privacy-policy.php',
+    '/ru/privacy-policy' => '/pages/ru/pages/privacy-policy.php',
+    '/ru/privacy-policy/' => '/pages/ru/pages/privacy-policy.php',
 
-    '/disclaimer' => 'pages/disclaimer.php',
-    '/disclaimer/' => 'pages/disclaimer.php',
-    '/ru/disclaimer' => 'pages/ru/pages/disclaimer.php',
-    '/ru/disclaimer/' => 'pages/ru/pages/disclaimer.php',
+    '/disclaimer' => '/pages/disclaimer.php',
+    '/disclaimer/' => '/pages/disclaimer.php',
+    '/ru/disclaimer' => '/pages/ru/pages/disclaimer.php',
+    '/ru/disclaimer/' => '/pages/ru/pages/disclaimer.php',
 
-    '/eb2s' => 'pages/eb-2-scientist.php',
-    '/eb2s/' => 'pages/eb-2-scientist.php',
-    '/ru/eb2s' => 'pages/ru/pages/eb-2-scientist.php',
-    '/ru/eb2s/' => 'pages/ru/pages/eb-2-scientist.php',
+    '/eb2s' => '/pages/eb-2-scientist.php',
+    '/eb2s/' => '/pages/eb-2-scientist.php',
+    '/ru/eb2s' => '/pages/ru/pages/eb-2-scientist.php',
+    '/ru/eb2s/' => '/pages/ru/pages/eb-2-scientist.php',
 
 
     // ///// ru pages
@@ -123,13 +123,13 @@ $routes = [
 
 
 // Get a streaming URL
-$request_uri = $_SERVER['REQUEST_URI'];
+$request_uri = str_replace(SUBDIR, '', $_SERVER['REQUEST_URI']);
 
 // Search for a specific page in an array of routes
 if (array_key_exists($request_uri, $routes)) {
-    include($routes[$request_uri]);
+    include DOCUMENT_ROOT . SUBDIR . $routes[$request_uri];
 } else {
     // If not, then 404
-    include('pages/404.php');
+    include DOCUMENT_ROOT . SUBDIR . '/pages/404.php';
 }
 
